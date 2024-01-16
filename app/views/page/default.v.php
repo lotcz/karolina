e20a13079d5af44e29581a37fec756dbef309908
Karel Zavadil <mojemejly@centrum.cz>
2024-01-16 00:10:16 +0000

    map

--- app/views/page/default.v.php
+++ app/views/page/default.v.php
@@ -2,7 +2,7 @@
 
 	<nav class="navbar navbar-expand-sm bg-white border-bottom sticky-top">
 		<div class="container-md">
-			<a class="navbar-brand" href="/">Parcela <strong>na prodej</strong></a>
+			<!--a class="navbar-brand" href="/">Parcela <strong>na prodej</strong></a-->
 
 			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Otevřít menu">
 				<span class="navbar-toggler-icon"></span>
@@ -32,10 +32,19 @@
 			<div class="mx-auto my-5">
 				<img src="<?=$this->z->images->img('karolina.png', 'view')?>" class="rounded img-fluid">
 				<h1 class="m-3 my-5">Parcela <strong>na prodej</strong></h1>
-				<?php
-					//echo $intro->get('cosmetic_service_description');
-				?>
-				<div class="separator"></div>
+				<p>Přímo od majitele.</p>
+
+				<div class="map">
+					<iframe
+						src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d794.5310126096227!2d14.379463276140932!3d50.164675852724486!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNTDCsDA5JzUyLjgiTiAxNMKwMjInNTAuNSJF!5e1!3m2!1scs!2scz!4v1705363678519!5m2!1scs!2scz"
+						width="100%"
+						height="100%"
+						style="border:0;"
+						allowfullscreen=""
+						loading="lazy"
+						referrerpolicy="no-referrer-when-downgrade"
+					></iframe>
+				</div>
 			</div>
 		</div>
 
@@ -46,17 +55,27 @@
 			?>
 		</div>
 
-		<div id="cenik">
-			<?php
-				//$this->renderPartialView('pricelist', ['pricelist' => $pricelist]);
-			?>
+		<div id="kontakt">
+			<h2>Kontakt</h2>
+
+			<div class="row">
+				<div class="col-lg">
+					<address>
+						Pavel Kaňka<br>
+						<br>
+						<br>
+						<br>
+					</address>
+				</div>
+				<div class="col-lg">
+					<p>mail: <a href="mailto:zahradakarolina@gmail.com">zahradakarolina@gmail.com</a></p>
+					<p>tel: +420 123 456 789</p>
+					<p></p>
+				</div>
+			</div>
+
 		</div>
 
-		<div id="kontakt">
-			<?php
-				$this->renderPartialView('contact');
-			?>
-		</div>
 	</div>
 
 	<footer class="py-5 text-center">
