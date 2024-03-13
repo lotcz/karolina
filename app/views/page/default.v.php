62abbcfea87ca8b6e9440f4f6ea4440123db1ff3
Karel Zavadil <zavadil@incomaker.com>
2024-03-13 18:12:44 +0100

    v1 done

--- app/views/page/default.v.php
+++ app/views/page/default.v.php
@@ -11,13 +11,10 @@
 			<div class="collapse navbar-collapse" id="navbar">
 				<ul class="navbar-nav m-auto my-2 my-lg-0" style="--bs-scroll-height: 100px;">
 					<li class="nav-item">
-						<a class="nav-link" href="#parcela">Parcela</a>
+						<a class="nav-link" href="#pozemek">Pozemek</a>
 					</li>
 					<li class="nav-item">
 						<a class="nav-link" href="#galerie">Galerie</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#cena">Cena</a>
 					</li>
 					<li class="nav-item">
 						<a class="nav-link" href="#kontakt">Kontakt</a>
@@ -28,11 +25,11 @@
 	</nav>
 
 	<div class="main container-md">
-		<div id="parcela" class="text-center intro">
+		<div id="pozemek" class="text-center intro">
 			<div class="mx-auto my-5">
 				<div class="map">
 					<iframe
-						src="https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d16969.037397848915!2d14.364428472167381!3d50.164984032124465!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNTDCsDA5JzUyLjgiTiAxNMKwMjInNTAuNSJF!5e1!3m2!1scs!2scz!4v1705524283202!5m2!1scs!2scz"
+						src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d23314.76199587048!2d14.380431194776941!3d50.164795484159896!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNTDCsDA5JzUzLjIiTiAxNMKwMjInNTAuMiJF!5e1!3m2!1scs!2scz!4v1705793313132!5m2!1scs!2scz"
 						width="100%"
 						height="100%"
 						style="border:0;"
@@ -40,38 +37,45 @@
 						loading="lazy"
 						referrerpolicy="no-referrer-when-downgrade"
 					></iframe>
-
 				</div>
-				<h1 class="m-3 my-5">Parcela <strong>na prodej</strong></h1>
 
-				<p class="text-start">Přímo od majitele.</p>
+				<h1 class="m-3 my-5">Pozemek</h1>
+				<h1 class="m-3 my-5">ROZTOKY U PRAHY</h1>
 
-				<div class="row text-start">
-					<div class="col-md">
+				<div class="parameters row justify-content-md-center m-auto text-start">
+					<div class="col-md-6">
 						<div class="row">
 							<div class="col">
-								Celková výměra
+								Druh pozemku
 							</div>
 							<div class="col">
-								<strong>Hodnota</strong>
+								<strong>Stavby pro rodinnou rekreaci</strong>
 							</div>
 						</div>
 						<div class="row">
 							<div class="col">
-								Zastavitelná plocha
+								Plocha pozemku
 							</div>
 							<div class="col">
-								<strong>Hodnota</strong>
+								<strong>3&nbsp;000&nbsp;m<sup>2</sup></strong>
 							</div>
 						</div>
-					</div>
-					<div class="col-md">
 						<div class="row">
 							<div class="col">
-								Převýšení
+								Zastavitelnost budovami
 							</div>
 							<div class="col">
-								<strong>Hodnota</strong>
+								<strong>max. 50&nbsp;m<sup>2</sup></strong>
+								(KZB 0.1, nadzemní podlaží a podkroví)
+							</div>
+						</div>
+						<div class="row">
+							<div class="col">
+								Celková zastavitelnost
+							</div>
+							<div class="col">
+								<strong>max. 600&nbsp;m<sup>2</sup></strong>
+								(KZP 0.2)
 							</div>
 						</div>
 						<div class="row">
@@ -79,9 +83,37 @@
 								Inženýrské sítě
 							</div>
 							<div class="col">
-								<strong>Hodnota</strong>
+								<strong>elektřina, voda, kanalizace na hranici pozemku</strong>
 							</div>
 						</div>
+						<div class="row">
+							<div class="col">
+								Přístup
+							</div>
+							<div class="col">
+								místní komunikace,
+								ulice <strong>V Úvoze</strong>
+							</div>
+						</div>
+
+					</div>
+				</div>
+
+				<div class="my-5 row justify-content-md-center m-auto text-start">
+					<div class="col-md-6">
+						<p>
+							Krásný a jedinečný pozemek.
+							Je mírně svažitý jižním směrem a nabízí pěkný výhled na místní část Žalov a vzdálenou krajinu.
+							Na pozemku jsou desítky vzrostlých stromů a další druhově pestrá vegetace.
+							Jedná se o nejvýchodnější část rodinného pozemku, jehož celková výměra je 1.4&nbsp;ha a je znám pod názvem
+							<a target="_blank" href="https://www.facebook.com/farma.karolina">Zahrada Karolína</a>.
+						</p>
+						<p>
+							Pozemek se nachází v zastavitelném území města a v územním plánu je veden ze 4/5 jako
+							plochy RI (stavby pro rodinnou rekreaci) a z 1/5 jako plochy ZS (zahrady soukromé)
+							s optimistickým výhledem do budoucna na možnou změnu do ploch BI (plochy bydlení v rodinných domech).
+							Minimální výměra pozemku pro stavbu je stanovena na 3&nbsp;000&nbsp;m<sup>2</sup>.
+						</p>
 					</div>
 				</div>
 
@@ -95,27 +127,15 @@
 			?>
 		</div>
 
-		<div id="cena">
-			<h2>Cena</h2>
-
-		</div>
-
 		<div id="kontakt">
 			<h2>Kontakt</h2>
 
-			<div class="row">
-				<div class="col-lg">
-					<address>
-						Pavel Kaňka<br>
-						<br>
-						<br>
-						<br>
-					</address>
-				</div>
-				<div class="col-lg">
+			<div class="row justify-content-md-center m-auto text-start">
+				<div class="col-md-5">
+					<p>Pavel Kaňka</p>
 					<p>mail: <a href="mailto:zahradakarolina@gmail.com">zahradakarolina@gmail.com</a></p>
-					<p>tel: +420 123 456 789</p>
-					<p></p>
+					<p>Instagram: <a target="_blank" href="https://www.instagram.com/zahrada.karolina">@zahrada.karolina</a></p>
+					<p>Facebook: <a target="_blank" href="https://www.facebook.com/farma.karolina">farma.karolina</a></p>
 				</div>
 			</div>
 
